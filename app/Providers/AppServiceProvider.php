2ee25cb945fb043e87db8b0de7d360af2e4284cd
Edno Nunes Ferreira <ednonunes@gmail.com>
2016-06-05 22:39:14 -0300

    Chamando o menu no admin

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace Onicms\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use \Schema;
+use Onicms\Models\Admin\MenuAdmin as MenuAdmin;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if(!Schema::hasTable('users'))
+            return true;
+
+        // Aqui configuramos variáveis que irão ser usadas nas views:
+
+        // Configs do site para as views(tanto do site quanto do admin):
+        //$this->configuracoes = Configuracoes::find(1);
+
+        view()->composer('layouts.admin', function($view)
+        {
+            // Obtendo o menu do sistema e jogando para a view do admin:
+            $menu          = MenuAdmin::orderBy('peso')->get()->toHierarchy();
+            $view->with( array('menu'          => $menu
+            ));
+        });
     }
 
     /**
--- resources/views/admin/_partes/sidebar.blade.php
+++ resources/views/admin/_partes/sidebar.blade.php
@@ -3,6 +3,13 @@
         <a href="{{ url('/admin') }}"><img src="{{ asset('assets/admin/img/logo-oni-white.svg') }}" alt="logo" class="img-responsive center-block" width="120" height="40"></a>
     </div>
     <ul class="nav">
-        <li><a href="{{ url('admin/logout') }}"><i class="fa fa-logout"></i> Sair</a></li>
+    	@if(isset($menu))
+	    	@foreach($menu as $m)
+		    	{!! renderizarMenuAdmin($m) !!}
+		  	@endforeach
+		@endif
+        @if( Auth::user()->email == 'admin@onidigital.com' )
+            <li><a href="{{ url('admin/menu_admin/create') }}">Cadastrar menus</a></li>
+	  	@endif
 	</ul>
 </div>
