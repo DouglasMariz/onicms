ce50ed783ee6ef64c0468e3238431e2281d591ee
Edno Nunes Ferreira <edno@onidigital.com>
2016-06-10 17:31:56 -0300

    Obtendo o menu clicado para manipular o menu(collapse in, active....)

--- app/Models/Admin/MenuAdmin.php
+++ app/Models/Admin/MenuAdmin.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 use Baum\Node; //https://github.com/etrepat/baum
 
+use Route;
+
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class MenuAdmin extends Node
@@ -33,6 +35,8 @@ class MenuAdmin extends Node
     // renderiza o menu do admin
 	public function renderizarMenuAdmin($menu) {
 
+		$menu_clicado = $this->get_menu_clicado();
+
 	    $icone = '';
 	    if(!empty($menu->icone))
 	    	$icone = '<i class="'.$menu->icone.'" ></i> '.PHP_EOL;
@@ -42,15 +46,23 @@ class MenuAdmin extends Node
 	    	$href = url($menu->url);
 
 	    $class = '';
+	    $collapse = '';
 
 	    if( $menu->isLeaf() ) {
 
-	      return '<li><a href="'.$href.'">' .$icone. $menu->nome . '</a></li>'.PHP_EOL;
+	    	if(isset($menu_clicado->id) && $menu_clicado->id == $menu->id)
+	    		$class = 'active';
+	      	return '<li class="'.$class.'" ><a href="'.$href.'">' .$icone. $menu->nome . '</a></li>'.PHP_EOL;
 
 	    } else {
+
+	      // Verifica se este menu parent é pai do menu clicado, para ficar collapse in:
+	      if(isset($menu_clicado->id) && ($menu_clicado->parent()->first()->id == $menu->id) )
+	      		$collapse = 'in';
+
 	      $html = '<li><a href="#submenu'.$menu->id.'" data-toggle="collapse" class="'.$class.'">' .$icone. $menu->nome.'</a>'.PHP_EOL;
 
-	      $html .= '<div class="collapse" id="submenu'.$menu->id.'">'.PHP_EOL;
+	      $html .= '<div class="collapse '.$collapse.'" id="submenu'.$menu->id.'">'.PHP_EOL;
 	      $html .= '<ul class="nav">'.PHP_EOL;
 
 	      foreach($menu->children as $filho)
@@ -62,6 +74,38 @@ class MenuAdmin extends Node
 	      $html .= '</li>'.PHP_EOL;
 	    }
 
-	  return $html;
+	  	return $html;
+	}
+
+	// Obtém o id do menu clicado pelo usuário
+	public function get_menu_clicado()
+	{
+		$rota = Route::getCurrentRoute()->getPath();
+		// as rotas salvas sempre tem '/' no final, então:
+		$rota = $rota.'/';
+		// previne duplicado:
+		$rota = str_replace("//", "/", $rota);
+		// obtém o menu desta rota:
+		$menu = MenuAdmin::where('url', '=', $rota)->first();
+		if(isset($menu->id)){
+			return $menu;
+		}
+		return false;
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
